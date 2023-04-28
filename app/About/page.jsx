6d9f415520060d23bbcc5e35196e54a3e3d98ccd
Jose Gustavo Valladares Barahona <gusbarahona01@gmail.com>
2023-04-28 14:39:15 -0600

    Mejorando redaccion parrafo

--- app/About/page.jsx
+++ app/About/page.jsx
@@ -16,7 +16,7 @@ export default function About(){
      </Head>
      <h1>About</h1>
      <p>Esta es una web simple de aprendizaje de Nextjs 13 con nuevas caracteristicas y funciones las cuales estan en fase beta, puedes visitar el sitio oficial de <Link href="https://nextjs.org/blog/next-13"> Nextjs</Link>. </p>
-      <p>Entre los cuales pongo en práctica: rutas estáticas, dinamicas, metadatas, api route, fetch, async, await, useRouter, rutas anidadas,paralelas,page de carga, use client, rutas ordenadas, componentes del lado del cliente y servidor.</p>
+      <p>Entre los conocimientos estan: rutas estáticas, dinamicas, metadatas, api route, fetch, async, await, useRouter, rutas anidadas,paralelas,page de carga, use client, rutas ordenadas, componentes del lado del cliente y servidor.</p>
      </>
    )
 }
